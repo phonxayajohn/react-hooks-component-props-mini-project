59f1965db742b8d6480fdb6a99b2267a0a75ae23
John Phonxaya <phonxayajohn@gmail.com>
2023-06-16 19:02:49 -0500

    mini project submission

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About({ image = "https://via.placeholder.com/215", about }) {
+    
+    return (
+        <aside>
+            <img src={image} alt="blog logo" />
+            <p>{about}</p>
+        </aside>
+    )
+}
